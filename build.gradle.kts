cc9c2f67ea679dbe12ba4556f709c9e91ce954aa
YCX-COMPANY <974577817@qq.com>
2020-07-06 18:03:32 +0800

    config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
 
-    val kotlin_version by extra("1.3.72")
+buildscript {
+    apply(from = file("mavenConfig.gradle.kts"))
     repositories {
+
         google()
         jcenter()
-        maven(url = "https://jitpack.io")
+        maven(url = extra.get("mavenUrl").toString()) {
+            credentials {
+                username = extra.get("mavenUserName").toString()
+                password = extra.get("mavenPassword").toString()
+            }
+        }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
-
+        classpath("son.ysy.plugin:libs:1.0.1")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
-includeBuild("D:/Code/Android/Plugin/plugin_libs")
-include ("app")
+include("app")
 include(":lib_base")
 include(":lib_common")
 rootProject.name = "YsyPhoto"
